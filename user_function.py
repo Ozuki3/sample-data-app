eb02b3e55c503061133351afc3f7dc21bd0dd983
kristen-wy <54915016+Ozuki3@users.noreply.github.com>
2021-03-21 19:43:07 -0700

    Add files via upload

---
+++ user_function.py
@@ -0,0 +1,29 @@
+# ######app user database######
+import sqlite3
+conn = sqlite3.connect('userdata.db',check_same_thread=False)
+c = conn.cursor()
+
+#app user password
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+
+#add user data into database
+def create_user():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+def add_user(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+	return True
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
